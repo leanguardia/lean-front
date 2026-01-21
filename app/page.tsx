a7981ff243cd620f82dcac19c442a1d87c68f67d
leanguardia <lean.guardia93@gmail.com>
2026-01-20 21:08:03 -0400

    BentoGrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,74 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="font-serif max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
+    <div className="py-4 px-4 sm:py-4 mx-auto min-h-screen">
+      <div className="grid grid-cols-1 sm:grid-cols-6 md:grid-cols-12 gap-5 auto-rows-[210px] min-h-screen">
+
+        <div className="xs:col-span-1 sm:col-span-6 md:row-span-1 md:col-span-3 lg:col-span-2 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Hero</h3>
           </a>
         </div>
-      </main>
+
+        <div className="xs:col-span-1 sm:col-span-6 md:row-span-1 md:col-span-5 lg:col-span-6 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Why?</h3>
+          </a>
+        </div>
+
+        <div className="xs:col-span-1 sm:col-span-6 md:col-span-4 md:row-span-3 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Interests</h3>
+          </a>
+        </div>
+
+        <div className="sm:col-span-6 md:col-span-4 md:row-span-2 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Chat with me</h3>
+          </a>
+        </div>
+
+        <div className="sm:col-span-6 md:col-span-4 md:row-span-3 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Institutions</h3>
+          </a>
+        </div>
+
+        <div className="sm:col-span-6 md:col-span-4 md:row-span-3 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Blog</h3>
+          </a>
+        </div>
+        <div className="sm:col-span-6 md:col-span-4 md:row-span-1 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">CV</h3>
+          </a>
+        </div>
+        <div className="sm:col-span-6 md:col-span-8 md:row-span-2 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Data</h3>
+          </a>
+        </div>
+        <div className="sm:col-span-6 md:col-span-7 md:row-span-1 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Spotify</h3>
+          </a>
+        </div>
+        <div className="sm:col-span-6 md:col-span-5 md:row-span-1 flex flex-col">
+          <a href="" className="group relative flex flex-col overflow-hidden rounded-lg px-4 pb-4 flex-grow">
+            <div className="absolute inset-0 bg-foreground"></div>
+            <h3 className="z-10 text-2xl font-medium text-white absolute top-0 left-0 p-4 xs:text-xl md:text-3xl">Socials</h3>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
